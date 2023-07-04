772c0e30f9e0100b06cd2dd8db490304c13810c5
aspirin2d <aspirin2d@outlook.com>
2023-07-04 23:47:22 +0800

    add more tests

--- memo/agent_handlers_test.go
+++ memo/agent_handlers_test.go
@@ -3,6 +3,7 @@ package memo
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http/httptest"
 	"testing"
 
@@ -13,14 +14,16 @@ import (
 	"golang.org/x/net/context"
 )
 
-type mockAgentsModel struct{}
+type mockAgentsModel struct {
+	Error error // error to return
+}
 
 func (mam *mockAgentsModel) Add(ctx context.Context, agent *Agent) (primitive.ObjectID, error) {
-	return primitive.NewObjectID(), nil
+	return primitive.NewObjectID(), mam.Error
 }
 
 func (mam *mockAgentsModel) Update(ctx context.Context, agent *Agent) error {
-	return nil
+	return mam.Error
 }
 
 func (mam *mockAgentsModel) Delete(ctx context.Context, id primitive.ObjectID) error {
@@ -28,7 +31,7 @@ func (mam *mockAgentsModel) Delete(ctx context.Context, id primitive.ObjectID) e
 }
 
 func (mam *mockAgentsModel) Get(ctx context.Context, id primitive.ObjectID) (*Agent, error) {
-	return nil, nil
+	return &Agent{Name: "aspirin2d"}, nil
 }
 
 func (mam *mockAgentsModel) List(ctx context.Context, offset primitive.ObjectID) ([]*Agent, error) {
@@ -56,6 +59,11 @@ func (s *AgentHandlersSuite) SetupTest() {
 	s.context, s.router = gin.CreateTestContext(s.writer)
 
 	s.router.POST("/add", s.memo.AddAgent)
+	s.router.GET("/get/:aid", s.memo.GetAgent)
+}
+
+func (s *AgentHandlersSuite) TearDownTest() {
+	s.memo.Agents.(*mockAgentsModel).Error = nil
 }
 
 func (s *AgentHandlersSuite) TestAddAgent() {
@@ -71,6 +79,42 @@ func (s *AgentHandlersSuite) TestAddAgent() {
 	s.NoError(err)
 	_, err = primitive.ObjectIDFromHex(m["inserted"].(string))
 	s.NoError(err)
+}
+
+func (s *AgentHandlersSuite) TestAddAgentWithError() {
+	s.memo.Agents.(*mockAgentsModel).Error = NewWrapError(400, errors.New("agent id should be nil"), "")
+
+	mbody := map[string]interface{}{
+		"id":   primitive.NewObjectID().Hex(),
+		"name": "aspirin2d",
+	}
+	body, _ := json.Marshal(mbody)
+
+	req := httptest.NewRequest("POST", "/add", bytes.NewReader(body))
+	s.router.ServeHTTP(s.writer, req)
+	var m map[string]interface{}
+	s.Equal(400, s.writer.Code)
+	_ = json.NewDecoder(s.writer.Body).Decode(&m)
+	s.Equal("agent id should be nil", m["msg"])
+}
+
+func (s *AgentHandlersSuite) TestGetAgent() {
+	req := httptest.NewRequest("GET", "/get/123", nil)
+	s.router.ServeHTTP(s.writer, req)
+	var m map[string]interface{}
+	s.Equal(400, s.writer.Code)
+	_ = json.NewDecoder(s.writer.Body).Decode(&m)
+	s.Equal("invalid agent id", m["msg"])
+
+	s.writer = httptest.NewRecorder()
+	req = httptest.NewRequest("GET", "/get/"+primitive.NewObjectID().Hex(), nil)
+	s.router.ServeHTTP(s.writer, req)
+	s.Equal(200, s.writer.Code)
+	s.T().Log(s.writer.Body.String())
+
+	var ag Agent
+	_ = json.NewDecoder(s.writer.Body).Decode(&ag)
+	s.Equal("aspirin2d", ag.Name)
 }
 
 func TestAgentHandlersSuite(t *testing.T) {
