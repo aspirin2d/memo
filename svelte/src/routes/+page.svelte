edc7230eb489a5b808f58ab99de1500450f90d0e
Aspirin2D <aspirin2d@outlook.com>
2023-07-18 11:34:54 +0800

    add tailwindcss

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+</script>
+
+<h1 class="bg-slate-200">Memo Debug Page</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+</style>
